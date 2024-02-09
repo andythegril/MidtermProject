e0f95623d8b718376288e7511de8d80ac1a12fee
manymoo <144646017+manymoo@users.noreply.github.com>
2024-02-10 00:02:50 +0700

    finish

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def load_map(map_path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--map', help='The map file', default='maps/demo.txt')
+    parser.add_argument('--map', help='The map file', default='maps/demo3.txt')
     parser.add_argument('--strategy', help='The strategy to solve the game', default='custom')
     args = parser.parse_args()
 
@@ -28,9 +28,10 @@ if __name__ == '__main__':
     # strategy = 'bfs'
     # strategy = 'dfs'
     # strategy = 'dfs_limited_depth'
-    # strategy = 'astar'
+    strategy = 'astar'
     # strategy = 'ucs'
-    strategy = 'greedy'
+    # strategy = 'greedy'
+    # strategy = 'custom'
     print(f"Using strategy: {strategy}")
     solver = Solver(game_state, strategy)
     solver.solve()
